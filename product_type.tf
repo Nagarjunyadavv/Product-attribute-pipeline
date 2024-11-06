4a6fba7b41c991a138ec40df1b0d25ac20689f66
Nagarjunyadavv <nagarjunyadav.vangu@hcltech.com>
2024-11-06 10:14:21 +0530

    Update product_type.tf New Name with latest date and version

--- product_type.tf
+++ product_type.tf
@@ -1,6 +1,6 @@
-resource "commercetools_product_type" "my-product-type-051124-1" {
-  key         = "my-product-type-key-051124-1"
-  name        = "Lens specification-051124-1"
+resource "commercetools_product_type" "my-product-type-061124-1" {
+  key         = "my-product-type-key-061124-1"
+  name        = "Lens specification-061124-1"
   description = "All the specific info concerning the lens"
 
   attribute {
