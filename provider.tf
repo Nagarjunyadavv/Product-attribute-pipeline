49a766bb5db0ae7e218f4627e10e7adadd241bc6
Nagarjunyadavv <nagarjunyadav.vangu@hcltech.com>
2024-11-06 11:46:20 +0530

    Update provider.tf Removed the Secrets from the git repo

--- provider.tf
+++ provider.tf
@@ -9,9 +9,6 @@ terraform {
 
 provider "commercetools" {
   # Configuration options
-    project_key= "naga-demo-project1"
-    client_id= "0ragSXyuy3MBZfCnUicQ33Js"
-    client_secret= "V3WQoYs-LyhdgFcCMv2j8OEOI8e_cJRw"
     token_url= "https://auth.us-central1.gcp.commercetools.com"
     api_url= "https://api.us-central1.gcp.commercetools.com"
     scopes= "manage_project:naga-demo-project1 view_audit_log:naga-demo-project1 manage_api_clients:naga-demo-project1 view_api_clients:naga-demo-project1"
