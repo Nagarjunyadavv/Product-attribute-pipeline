839006e7b28bc47dd6e667617945290fcb36093a
Nagarjunyadavv <nagarjunyadav.vangu@hcltech.com>
2024-12-10 12:45:01 +0530

    New Project Attribute Testing-01
    Added New Project details

--- provider.tf
+++ provider.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "commercetools" {
   # Configuration options
-    project_key= "naga-demo-project1"
-    client_id= "0ragSXyuy3MBZfCnUicQ33Js"
-    client_secret= "V3WQoYs-LyhdgFcCMv2j8OEOI8e_cJRw"
+    project_key= "test-project-x1"
+    client_id= "2c_aIDQQZ-16ZRoHfHpeyRYd"
+    client_secret= "iuct7WZkOLyS6i2rZNwxQ-ZmxvvTrbFe"
     token_url= "https://auth.us-central1.gcp.commercetools.com"
     api_url= "https://api.us-central1.gcp.commercetools.com"
-    scopes= "manage_project:naga-demo-project1 view_audit_log:naga-demo-project1 manage_api_clients:naga-demo-project1 view_api_clients:naga-demo-project1"
+    scopes= "manage_project:test-project-x1 view_audit_log:test-project-x1 manage_api_clients:test-project-x1 view_api_clients:test-project-x1"
 
 }
